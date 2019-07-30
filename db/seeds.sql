36a6b8166b5b2df4b1fc24e31e6c2117b6930909
Tyler Cornwell <tylercornwell@hotmail.com>
2019-07-30 10:42:21 -0700

    turning in homework

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES 
+        ('Cheese Burger', FALSE), ('Double Take', FALSE),
+        ('Triple Take', FALSE), ('4 x 4', FALSE),
+        ('Big Mac', FALSE), ('Whopper', FALSE),
+        ('Half Pounder', FALSE), ('Full Pounder', FALSE),
+        ('Quarter Pounder', FALSE), ('3/4ths Pounder', FALSE),
+        ('Egg Burger', FALSE), ('Bacon Burger', FALSE),
+        ('Chili Cheese Burger', FALSE), ('French Fry Burger', FALSE)
+        
+SELECT * FROM burgers;
